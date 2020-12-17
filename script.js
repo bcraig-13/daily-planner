332783b5865e9b1a5b7e7f53047b8e4f01c4b99b
Brandon Craig <thegamingraven13@gmail.com>
2020-12-17 15:07:36 -0800

    User can save input value to local storage, but I can't retrieve it on page load. Ran out of time.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var currentDayEl = $("#currentDay");
+currentDayEl.text(moment().format("MMM Do YY"));
 var currentTimeEl = parseInt(moment().format('k'));
 
-currentDayEl.text(moment().format("MMM Do YY"));
 //get local storage on page load
 //find way to save each line in local storage
 
@@ -21,13 +21,26 @@ for(var hourCount = 9; hourCount < 18; hourCount++) {
         hourGrabEl.attr("class", "row time-block future")
     }
 }
-    
-$(".saveBtn").on("click"), function () {
-    
-}
-
+// V--Possible solution?
 // $(".saveBtn").on("click", function () {
 //     var tempString = $(this.previousElementSibling).val();
-//         localStorage.setItem("description", JSON.stringify.tempString);
-
+//     var hourTemp = $(this).val();
+//     var temp object
+//     localStorage.setItem("description", JSON.stringify(tempString));
 // })
+
+$(".btn").on("click", function() {
+    var save = document.getElementsByTagName("textarea").value;
+    localStorage.setItem("save", save);
+})
+
+
+//Select save btn for 0900 and listen for clicks
+//Select content from 0900 block
+//Move selected content to local storage
+//0900 key, moved to local storage
+
+//On page load, retrieve local storage from 0900 block
+//If something was retrieved, then set value
+//Set the value of the 0900 text area
+
